f34ff3a137c7a11a1a1dd3e6b2fe6b0aabdd0442
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-10-08 16:14:36 -0700

    getting parameters from AWS Parameter Store 5

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# Update the system
-yum update -y
+# # Update the system
+# yum update -y
 
-# Install Docker
-yum install -y docker
+# # Install Docker
+# yum install -y docker
 
-# Start the Docker service
-service docker start
+# # Start the Docker service
+# service docker start
 
-# Clean up old containers
-docker stop my_container || true
-docker rm my_container || true
+# # Clean up old containers
+# docker stop my_container || true
+# docker rm my_container || true
 
-# Ensure there's enough disk space
-df -h
+# # Ensure there's enough disk space
+# df -h
