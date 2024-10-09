62d906c7729bee13fcb6498b4025a526a62b6b36
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-10-09 11:51:44 -0700

    allowed all host

--- event_management/settings.py
+++ event_management/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = 'django-insecure-9$3aol90rpnqww4_54c8q0%enfvkv13#=pgbc!o97kwht5mgst
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '35.153.182.217']
+ALLOWED_HOSTS = ['localhost', '35.153.182.217', '*']
 
 
 
