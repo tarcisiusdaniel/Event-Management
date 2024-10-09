2a160c5e3a56e28a589283e8df00895204e4483e
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-10-08 20:00:38 -0700

    getting parameters from AWS Parameter Store 10

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -20,13 +20,13 @@
 export AWS_REGION="us-east-1"  # Adjust as necessary
 
 # Fetch parameters from Parameter Store and export them as environment variables
-export GOOGLE_OAUTH_CLIENT_ID=$(aws ssm get-parameter --name "GOOGLE_OAUTH_CLIENT_ID" --with-decryption --query "Parameter.Value" --output text)
-export GOOGLE_OAUTH_CLIENT_SECRET=$(aws ssm get-parameter --name "GOOGLE_OAUTH_CLIENT_SECRET" --with-decryption --query "Parameter.Value" --output text)
-export DB_NAME=$(aws ssm get-parameter --name "DB_NAME" --with-decryption --query "Parameter.Value" --output text)
-export DB_USER=$(aws ssm get-parameter --name "DB_USER" --with-decryption --query "Parameter.Value" --output text)
-export DB_PASSWORD=$(aws ssm get-parameter --name "DB_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
-export DB_HOST=$(aws ssm get-parameter --name "DB_HOST" --with-decryption --query "Parameter.Value" --output text)
-export JWT_TOKEN=$(aws ssm get-parameter --name "JWT_SECRET" --with-decryption --query "Parameter.Value" --output text)
+export GOOGLE_OAUTH_CLIENT_ID=$(aws ssm get-parameter --name "/event_management_backend/GOOGLE_OAUTH_CLIENT_ID" --with-decryption --query "Parameters.Value" --output text)
+export GOOGLE_OAUTH_CLIENT_SECRET=$(aws ssm get-parameter --name "/event_management_backend/GOOGLE_OAUTH_CLIENT_SECRET" --with-decryption --query "Parameters.Value" --output text)
+export DB_NAME=$(aws ssm get-parameter --name "/event_management_backend/DB_NAME" --with-decryption --query "Parameters.Value" --output text)
+export DB_USER=$(aws ssm get-parameter --name "/event_management_backend/DB_USER" --with-decryption --query "Parameters.Value" --output text)
+export DB_PASSWORD=$(aws ssm get-parameter --name "/event_management_backend/DB_PASSWORD" --with-decryption --query "Parameters.Value" --output text)
+export DB_HOST=$(aws ssm get-parameter --name "/event_management_backend/DB_HOST" --with-decryption --query "Parameters.Value" --output text)
+export JWT_TOKEN=$(aws ssm get-parameter --name "/event_management_backend/JWT_SECRET" --with-decryption --query "Parameters.Value" --output text)
 
 # Ensure Docker is running
 # sudo service docker start
