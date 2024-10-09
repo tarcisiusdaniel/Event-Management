69008550476e7cd531b259b86d8957d0ad218038
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-10-08 22:18:09 -0700

    getting parameters from AWS Parameter Store 22

--- deployment_scripts/start_application.sh
+++ deployment_scripts/start_application.sh
@@ -19,4 +19,4 @@ export DB_HOST=$(aws ssm get-parameter --name "/event_management_backend/DB_HOST
 export JWT_TOKEN=$(aws ssm get-parameter --name "/event_management_backend/JWT_SECRET" --query "Parameters[0].Value" --output text)
 
 # Start Docker containers
-docker-compose up --build -d
+docker-compose up --build
