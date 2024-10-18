4c87c194418f59560fd993994361259046ceb0bb
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-10-18 01:42:04 -0700

    changes for ci cd t4

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -3,13 +3,15 @@
 echo "Starting Docker installation..."
 
 # Install Docker
-sudo amazon-linux-extras install docker -y
+sudo yum update  -y
+sudo amazon-linux-extras install docker
+sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d '\"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Finished Docker installation..."
