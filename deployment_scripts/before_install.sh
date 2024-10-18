1463413e1e38af0f1c420f0b297ea60587d1ab9a
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-10-18 01:16:45 -0700

    changes for ci cd t2

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,33 +1,11 @@
 #!/bin/sh
 
-# # Update the system
-# yum update -y
-
-# # Install Docker
-# yum install -y docker
-
-# # Start the Docker service
-# service docker start
-
-# # Clean up old containers
-# docker stop my_container || true
-# docker rm my_container || true
-
-# # Ensure there's enough disk space
-# df -h
-
-# Update package information
-sudo yum update -y
-
-# Install Docker (for Amazon Linux 2)
-sudo amazon-linux-extras install docker
-
-# Start Docker service
-sudo service docker start
-
-# Add ec2-user to Docker group
+# Install Docker
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d '\"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
